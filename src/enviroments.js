8f29f7a80925f2e6a82b84ee9a8cc2a5d8a837f1
Maria Perez Cobo <mariaperezcobo@hotmail.com>
2024-01-19 06:10:58 -0300

    arq prueba mil

--- src/enviroments.js
+++ src/enviroments.js
@@ -1,18 +1,38 @@
+
 import {config} from 'dotenv'
 import path from 'path'
 import __dirname from './utils.js'
 
 const envPath= path.resolve(__dirname, '../.env')
-console.log(envPath)
+
+console.log('Before loading dotenv:', process.env.MONGO_URL, process.env.MONGO_DBNAME);
 
 config({path: envPath})
 
-const PORT = process.env.PORT
-const NODE_ENV = process.env.NODE_ENV
-const MONGO_URL = process.env.MONGO_URL
-const MONGO_DBNAME = process.env.MONGO_DBNAME
+// // const PORT = process.env.PORT
+// // const NODE_ENV = process.env.NODE_ENV
+// // const MONGO_URL = process.env.MONGO_URL
+// // const MONGO_DBNAME = process.env.MONGO_DBNAME
 
-//console.log(process.env.MONGO_URL);
-//console.log(process.env.MONGO_DBNAME);
-//console.log(process.env);
-console.log('File loaded');
+// // dotenv.config({ path: envPath });
+
+
+// console.log('File loaded!!');
+
+const environmentConfig = {
+    PERSISTENCE: process.env.PERSISTENCE,
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    MONGO_URL: process.env.MONGO_URL,
+    MONGO_DBNAME: process.env.MONGO_DBNAME,
+  };
+  
+  export default environmentConfig
+  
+
+// console.log('process.env.MONGO_UR', process.env.MONGO_URL);
+// console.log('environmentConfig.MONGO_URL', environmentConfig.MONGO_URL);
+// console.log('port', process.env.PORT);
+// console.log('port', environmentConfig.PORT);
+// console.log(process.env.MONGO_DBNAME);
+// //console.log(process.env);
